47c4e0042dc16c65879f72ce00f8c0f3906f90f2
Raza <mahbub.razashihab@gmail.com>
2024-01-07 13:56:32 +0800

    working on heroes component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,14 @@
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
+
 const MarketingPage = () => {
   return (
-    <>
-      <div>Marketing Page</div>
-    </>
+    <div className='flex min-h-full flex-col'>
+      <div className='flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start'>
+        <Heading />
+        <Heroes />
+      </div>
+    </div>
   );
 };
 
