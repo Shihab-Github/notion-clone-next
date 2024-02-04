938751e64d0769c64c5881442081934d7c8405a8
Raza <mahbub.razashihab@gmail.com>
2024-02-04 23:38:52 +0800

    mobile navbar in progress

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,13 +1,38 @@
 "use client";
-
-import { ChevronLeft } from "lucide-react";
+import { ElementRef, useRef, useState } from "react";
+import { ChevronsLeft } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export default function Navigation() {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
-      <aside className='group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary'>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "w-0"
+        )}
+      >
         <div role='button'>
-          <ChevronLeft className='absolute right-3 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300  group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600' />
+          <ChevronsLeft
+            className={cn(
+              "absolute right-3 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300  group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600",
+              isMobile && "opacity-100"
+            )}
+          />
         </div>
         <div>
           <p>Action Items</p>
