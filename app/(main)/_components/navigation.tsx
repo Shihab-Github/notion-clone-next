3ed3bb1f1ada0b275452fa17c58d9833ac1f0a63
Raza <mahbub.razashihab@gmail.com>
2024-02-04 21:05:09 +0800

    added collapse icon to sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { ChevronLeft } from "lucide-react";
+
 export default function Navigation() {
   return (
     <>
       <aside className='group/sidebar relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary'>
+        <div role='button'>
+          <ChevronLeft className='absolute right-3 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-neutral-300  group-hover/sidebar:opacity-100 dark:hover:bg-neutral-600' />
+        </div>
         <div>
           <p>Action Items</p>
         </div>
