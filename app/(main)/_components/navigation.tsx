60cc697d7be4d12dc4da7c04d73e991ce1147863
Raza <mahbub.razashihab@gmail.com>
2024-02-17 17:30:57 +0800

    added burger menu for mobile devices

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { ElementRef, useRef, useState } from "react";
-import { ChevronsLeft } from "lucide-react";
+import { ElementRef, useEffect, useRef, useState } from "react";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { useMediaQuery } from "usehooks-ts";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
@@ -15,6 +15,10 @@ export default function Navigation() {
 
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  useEffect(() => {
+    setIsCollapsed((prev) => !prev);
+  }, [isMobile]);
 
   return (
     <>
@@ -42,6 +46,20 @@ export default function Navigation() {
         </div>
         <div className='absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100' />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute left-60 top-0 z-[99999] w-[calc(100%-240px)]",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "left-0 w-full"
+        )}
+      >
+        <nav className='w-full bg-transparent px-3 py-2'>
+          {isCollapsed && (
+            <MenuIcon role='button' className='h-6 w-6 text-muted-foreground' />
+          )}
+        </nav>
+      </div>
     </>
   );
 }
